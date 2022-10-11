3993ce24953bff2af085b56029ce1570db7684c1
Suprun Oleksandr <suprun.energoatom@gmail.com>
2022-10-11 17:23:32 +0300

    add operation functions

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
-const app = require('./app')
+const app = require("./app");
+const func = require("./models/contacts");
 
 app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+  console.log("Server running. Use our API on port: 3000");
+});
+
+const f = async () => {
+  const result = await func.updateContact(1, {
+    id: "4",
+    name: "Wylie Pope",
+    email: "est@utquamvel.net",
+    phone: "(692) 802-2949",
+  });
+  console.log(result);
+  return result;
+};
+f();
